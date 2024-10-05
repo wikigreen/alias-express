7eb3a8f48ce3dbe03c33b9c2a631c753eda32338
Volodymyr Hrynevych <vhrynevich20@gmail.com>
2024-10-05 06:22:14 +0000

    Add socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Express + +");
+const server = createServer(app);
+const socketio = new Server(server, {
+  cors: {
+    origin: "*", // Allow requests from any origin
+    methods: ["GET", "POST"],
+  },
 });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+const port = process.env.PORT || 3000;
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("Express + +");
+});
+
+socketio.on("connection", (socket) => {
+  console.log("new client connection" + socket.id);
+
+  // Respond to custom events
+  socket.on("message", (data) => {
+    console.log(`Message from ${socket.id}:`, data);
+    socket.emit("response", { message: "Message received!" });
+  });
+
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
