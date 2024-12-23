99999a3e580eae5db08ca0368c8d4c0a7a7ea8e1
Volodymyr Hrynevych <vhrynevich20@gmail.com>
2024-12-23 20:34:28 +0000

    Create wordsService with mocked methods

--- backend/src/words/wordsService.ts
+++ backend/src/words/wordsService.ts
@@ -3,17 +3,15 @@ import { v4 as uuid } from "uuid";
 class WordsService {
   wordMap = {} as Record<string, string>;
 
+  initWords(gameId: string) {
+    this.wordMap[gameId] = uuid();
+  }
+
   getCurrentWord(gameId: string) {
-    if (this.wordMap[gameId] == null) {
-      this.wordMap[gameId] = uuid();
-    }
     return this.wordMap[gameId];
   }
 
   getCurrentAndNextWords(gameId: string) {
-    if (this.wordMap[gameId] == null) {
-      this.wordMap[gameId] = uuid();
-    }
     const currentWord = this.wordMap[gameId];
     this.wordMap[gameId] = uuid();
     const nextWord = this.wordMap[gameId];
