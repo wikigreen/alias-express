f6e9b671a31eade5aacb65144a2b503c62700e5f
Volodymyr Hrynevych <vhrynevich20@gmail.com>
2024-12-25 09:26:33 +0000

    Setup api client for words

--- frontend/src/features/Game/services/gameApi.ts
+++ frontend/src/features/Game/services/gameApi.ts
@@ -5,6 +5,13 @@ export const gameApi = createApi({
   reducerPath: "gameApi",
   baseQuery: fetchBaseQuery({ baseUrl: "/api/game" }),
   endpoints: (builder) => ({
+    getWord: builder.query<string, { roomId: string; gameId: string }>({
+      query: (body) => ({
+        url: "guess",
+        method: "POST",
+        body,
+      }),
+    }),
     createGame: builder.mutation<void, GameSettings>({
       query: (body) => ({
         url: "",
@@ -41,7 +48,7 @@ export const gameApi = createApi({
       }),
     }),
     makeGuess: builder.mutation<
-      void,
+      string,
       { roomId: string; gameId: string; guessed: boolean }
     >({
       query: (body) => ({
@@ -49,6 +56,24 @@ export const gameApi = createApi({
         method: "POST",
         body,
       }),
+      async onQueryStarted(
+        { gameId, roomId },
+        { dispatch, queryFulfilled, getState },
+      ) {
+        const productIds = gameApi.util.selectCachedArgsForQuery(
+          getState(),
+          "getWord",
+        );
+        queryFulfilled
+          .then(({ data }) => {
+            productIds
+              .filter((p) => p.gameId === gameId && p.gameId === roomId)
+              .forEach((p) => {
+                dispatch(gameApi.util.upsertQueryData("getWord", p, data));
+              });
+          })
+          .catch((e) => console.error(e));
+      },
     }),
   }),
 });
