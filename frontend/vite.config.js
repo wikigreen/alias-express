eac4be9a8fe9aec9d12ffe9d0d00a5dfef1a6a8b
Volodymyr Hrynevych <vhrynevich20@gmail.com>
2024-12-27 07:54:16 +0000

    Updates after build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     server: {
+        port: 5173,
         proxy: {
             "/socket.io": {
                 target: "http://localhost:3000", // Your backend server running Socket.IO
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4342,
+    port: 5173,
     proxy: {
       "/socket.io": {
         target: "http://localhost:3000", // Your backend server running Socket.IO
